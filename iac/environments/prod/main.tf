de49fb6b1729627c3255dc5cf2adcce8f798d7f3
Junior David Infantes Rondo <juniordavidinfantesrondo15@gmail.com>
2026-06-10 17:53:49 -0500

    feat(iac): implementacion de infraestructura para hosting en AWS (S3 y CloudFront)

---
+++ iac/environments/prod/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "hosting" {
+  source      = "../../modules/hosting"
+  prefix      = "academia-tesla-hub"
+  environment = "prod"
+}
