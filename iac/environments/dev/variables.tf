9672c2197e4a10adc83dd54ab02e826047870658
Junior David Infantes Rondo <juniordavidinfantesrondo15@gmail.com>
2026-06-11 22:56:25 -0500

    feat: Configuración de variables y providers

---
+++ iac/environments/dev/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "Region de AWS"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "prefix" {
+  description = "Prefijo para los recursos"
+  type        = string
+  default     = "academia-tesla-hub"
+}
